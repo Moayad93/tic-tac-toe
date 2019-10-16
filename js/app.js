6c6cfdff9d9d14a06cfce602b974a3cfad68262a
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-10-16 13:11:58 +0300

    #5 the project is 99.99% completed

--- js/app.js
+++ js/app.js
@@ -7,52 +7,53 @@ $(document).ready(function () {
 
   // Declare a variable for "td"s i.e. boxes class and variables for each "td"'s id
   const
-    $body           = $('body')
-    $header         = $('header'),
-    $main           = $('main'),
-    $box            = $('.box'),
-    $boxText        = $('.box').text(),
-    $reset          = $('#reset'),
-    $welcome        = $('#welcome')
-    $welcomeDiv     = $('#welcome div'),
-    $row1_col1      = $('#col-1-1'),
-    $row1_col2      = $('#col-1-2'),
-    $row1_col3      = $('#col-1-3'),
-    $row2_col1      = $('#col-2-1'),
-    $row2_col2      = $('#col-2-2'),
-    $row2_col3      = $('#col-2-3'),
-    $row3_col1      = $('#col-3-1'),
-    $row3_col2      = $('#col-3-2'),
-    $row3_col3      = $('#col-3-3');
-    $footer         = $('footer');
+    $body = $('body')
+  $header = $('header'),
+    $main = $('main'),
+    $box = $('.box'),
+    $boxText = $('.box').text(),
+    $clickedBoxes = [],
+    $reset = $('#reset'),
+    $welcome = $('#welcome')
+  $welcomeDiv = $('#welcome div'),
+    $row1_col1 = $('#col-1-1'),
+    $row1_col2 = $('#col-1-2'),
+    $row1_col3 = $('#col-1-3'),
+    $row2_col1 = $('#col-2-1'),
+    $row2_col2 = $('#col-2-2'),
+    $row2_col3 = $('#col-2-3'),
+    $row3_col1 = $('#col-3-1'),
+    $row3_col2 = $('#col-3-2'),
+    $row3_col3 = $('#col-3-3');
+  $footer = $('footer');
 
   // Declare a variable for player's turn xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
   let
-  $isXPlayerTurn    = true,
-  $xPlayerScore     = 0,
-  $oPlayerScore     = 0,
-  $xPlayerScoreText = $('#xScoreText'),
-  $oPlayerScoreText = $('#oScoreText'),
-  $turnMessage      = $('#turnMessage'),
-  $winMessage       = $('#winMessage');
+    $isXPlayerTurn = true,
+    $xPlayerScore = 0,
+    $oPlayerScore = 0,
+    $xPlayerScoreText = $('#xScoreText'),
+    $oPlayerScoreText = $('#oScoreText'),
+    $turnMessage = $('#turnMessage'),
+    $winMessage = $('#winMessage');
 
   // Fade in welcome content
   $welcomeDiv.fadeIn('slow');
 
   // Fade out welcome div
-  fadeWelcomeDiv = function() {
+  fadeWelcomeDiv = function () {
     $welcome.fadeOut('slow');
   }
 
   $welcome.click(fadeWelcomeDiv);
 
   // Validation function
-  const validation = function() {
-
+  const validation = function () {
+    console.log('validation');
     for (let i = 1; i < 4; i++) {
-      // Check winner horizontally
-      if
-      (
+      console.log('For loop');
+      if (
+        // Check winner horizontally
         (eval("$row" + i + "_col1.text()") === 'X' && eval("$row" + i + "_col2.text()") === 'X' && eval("$row" + i + "_col3.text()") === 'X') ||
         (eval("$row" + i + "_col1.text()") === 'O' && eval("$row" + i + "_col2.text()") === 'O' && eval("$row" + i + "_col3.text()") === 'O') ||
 
@@ -90,11 +91,21 @@ $(document).ready(function () {
         return;
       }
     }
+    // setTimeout(switchTurn, 10);
+    switchTurn();
+  }
+
+  // Change turn
+  const switchTurn = function () {
+    console.log('switchTurn');
+    console.log('___________switchTurn___________');
+    $isXPlayerTurn = !$isXPlayerTurn;
   }
 
   // Click event
   const tdClick = function () {
     console.log("Click Works Man!");
+    $winMessage.css('display', 'none');
     ($isXPlayerTurn) ?
     $(this).text('X') && $turnMessage.text('It\'s O player turn').fadeIn('slow'):
     $(this).text('O') && $turnMessage.text('It\'s X player turn').fadeIn('slow');
@@ -102,16 +113,21 @@ $(document).ready(function () {
     // To remove click event from the "td"
     $(this).unbind();
 
+    $clickedBoxes.push($(this));
+    console.log($clickedBoxes);
+
     validation();
 
     // Magic line to swith turns
-    $isXPlayerTurn = !$isXPlayerTurn;
+    // setTimeout(switchTurn, 0);
+
   }
 
   $box.click(tdClick);
 
   // Hover event
-  const tdHover = function() {
+  const tdHover = function () {
+    console.log('tdHover');
     $(this).css('transform', 'scale(1.1)');
     ($isXPlayerTurn) ? $(this).text('X') : $(this).text('O');
   }
@@ -119,7 +135,8 @@ $(document).ready(function () {
   $box.hover(tdHover);
 
   // Mouse leave event
-  const tdMouseLeave = function() {
+  const tdMouseLeave = function () {
+    console.log('tdMouseLeave');
     $(this).css('transform', 'scale(1)');
     $(this).unbind('mouseenter mouseleave', $(this).text(''));
   }
@@ -128,8 +145,22 @@ $(document).ready(function () {
 
   // Reset event
   const tdReset = function () {
-    $box.text(null);
-    $isXPlayerTurn = true;
+    console.log('tdReset');
+    console.log('____________tdReset__________');
+
+    $box.each(function () {
+      console.log('entered box each');
+
+      if ($(this).text() === '') {
+        console.log('checked boxed by include');
+        $(this).unbind();
+      }
+
+    });
+
+    $box.text('');
+
+    switchTurn();
     $turnMessage.text('');
     $box.click(tdClick);
     $box.hover(tdHover);
@@ -139,20 +170,32 @@ $(document).ready(function () {
   $reset.click(tdReset);
 
   // Change themes
-  const blueGrayTheme = function() {
+  const blueGrayTheme = function () {
     $body.css('color', '#FAFAFA');
     $header.css('background-color', '#212121');
     $main.css('background-color', '#263238');
     $footer.css('background-color', '#212121');
+
+    // $box.hover(function() {
+    //   $(this).css('background-color','#B0BEC5');
+    // }, function() {
+    //   $(this).css('background-color','transparent');
+    // });
   }
 
   $('#blueGrayButton').click(blueGrayTheme);
 
-  const offWhiteTheme = function() {
+  const offWhiteTheme = function () {
     $body.css('color', '#212121');
     $header.css('background-color', '#FAFAFA');
     $main.css('background-color', '#ECEFF1');
     $footer.css('background-color', '#FAFAFA');
+
+    // $box.hover(function() {
+    //   $(this).css('background-color','#B0BEC5');
+    // }, function() {
+    //   $(this).css('background-color','transparent');
+    // });
   }
 
   $('#offWhiteButton').click(offWhiteTheme);
